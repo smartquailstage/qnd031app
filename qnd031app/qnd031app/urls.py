5762c1fff8318eccfa301969db4bd9d0ffdbc266
smartquailstage <smartquail.stage@gmail.com>
2025-05-22 03:00:08 -0500

    Update urls.py

--- qnd031app/qnd031app/urls.py
+++ qnd031app/qnd031app/urls.py
@@ -55,7 +55,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ] + i18n_patterns(
-        path("sistema/", admin.site.urls),
+        path("inicio/", admin.site.urls),
     )
 
 # Add static URLs to the urlpatterns if in debug mode
