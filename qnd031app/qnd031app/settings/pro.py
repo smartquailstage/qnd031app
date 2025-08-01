0a5fe180e4dafb893b92840fe3a814fc76dba54e
smartquailstage <smartquail.stage@gmail.com>
2025-08-01 13:13:59 -0500

    sss

--- qnd031app/qnd031app/settings/pro.py
+++ qnd031app/qnd031app/settings/pro.py
@@ -172,21 +172,3 @@ TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_WHATSAPP_FROM = os.getenv("TWILIO_WHATSAPP_FROM")
 
 
-from .cdn.conf import * #noqa
-# Configuración de AWS
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL")  # Cambia si usas otro endpoint
-AWS_S3_OBJECT_PARAMETERS = {
-    "CacheControl": "max-age=86400", 
-    "ACL": "public-read"  # Cambia a 'private' si los archivos deben ser privados
-}
-
-# Configuración de almacenamiento
-AWS_LOCATION = os.environ.get("AWS_LOCATION")  # 'static' o 'media'
-
-
-# Rutas públicas a los archivos
-MEDIA_URL = "https://www-static.sfo3.digitaloceanspaces.com/media/"
-STATIC_URL = "https://www-static.sfo3.digitaloceanspaces.com/static/"
