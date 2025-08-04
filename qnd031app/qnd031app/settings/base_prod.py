78bcd6dcbbeaf3ef3131907979df985e0d3fb2f7
smartquailstage <smartquail.stage@gmail.com>
2025-08-03 19:38:32 -0500

    asdfadsc

--- qnd031app/qnd031app/settings/base_prod.py
+++ qnd031app/qnd031app/settings/base_prod.py
@@ -203,6 +203,7 @@ UNFOLD = {
         {"icon": "person", "title": _("Usuarios(AUTH)"), "link": reverse_lazy("admin:auth_user_changelist")},
         {"icon": "key", "title": _("Roles(RBAC)"), "link": reverse_lazy("admin:auth_group_changelist")},
         {"icon": "people", "title": _("Administrativos"), "link": reverse_lazy("admin:usuarios_administrativeprofile_changelist")},
+        {"icon": "people", "title": _("Comercial"), "link": reverse_lazy("admin:usuarios_perfil_comercial_changelist")},
         {"icon": "people", "title": _("Institucionales"), "link": reverse_lazy("admin:usuarios_perfilinstitucional_changelist")},
          {"icon": "people", "title": _("Terapeutas"), "link": reverse_lazy("admin:usuarios_perfil_terapeuta_changelist")},
         {"icon": "medical_services", "title": _("Servicios"), "link": reverse_lazy("admin:serviceapp_servicioterapeutico_changelist")},
