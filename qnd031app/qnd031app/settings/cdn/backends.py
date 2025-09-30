5f695c8b10f3d5a0263a8891133f6dbc04b4086c
smartquailstage <smartquail.stage@gmail.com>
2025-09-30 17:22:01 -0500

    dfsdfs

--- qnd031app/qnd031app/settings/cdn/backends.py
+++ qnd031app/qnd031app/settings/cdn/backends.py
@@ -2,6 +2,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticRootS3BotoStorage(S3Boto3Storage):
     location = "static"
+    file_overwrite = False
 
 class MediaRootS3BotoStorage(S3Boto3Storage):
     location = "media"
+    file_overwrite = False
