ef09214fcb389f6f37f2cadd6f517de6e6e7a8a2
smartquailstage <smartquail.stage@gmail.com>
2025-11-08 17:48:13 -0500

    Update urls.py

--- qnd031app/qnd031app/urls.py
+++ qnd031app/qnd031app/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
  #   path('appointment/', include('appointment.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
    # path('rosetta/', include('rosetta.urls')),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('usuarios.urls', namespace='usuarios')),
     #path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     #path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
