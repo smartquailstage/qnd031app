0b800a65d1128353129a2ddaf40691f166916835
smartquailstage <smartquail.stage@gmail.com>
2025-11-08 20:10:14 -0500

    Update environment.py

--- qnd031app/qnd031app/utils/environment.py
+++ qnd031app/qnd031app/utils/environment.py
@@ -6,7 +6,7 @@ def environment_callback(request):
     env = os.getenv('DJANGO_ENV', 'QND031MD(Desarollo)')
 
     if env == 'production':
-        return ["Meddes® (I+D+A)", "success"]
+        return ["Meddes® (I+D+A).V.0.3.0.1", "success"]
     elif env == 'staging':
         return ["Vesion.QND.0.3.1.0(Pruebas)", "warning"]
     elif env == 'demo':
